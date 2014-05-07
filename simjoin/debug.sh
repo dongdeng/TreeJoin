bf31a5b9ab25152a1d1a9691787288afb20b24fb
Zeyuan Shang <zeyuanxy@gmail.com>
2014-05-08 00:55:32 +0800

    compress dblp-100.xml

--- simjoin/debug.sh
+++
@@ -1,6 +0,0 @@
-g++ -g -c -o free.o free.cc
-g++ -g -c -o io.o io.cc
-g++ -g -c -o main.o main.cc
-g++ -g -c -o pre.o pre.cc
-g++ -g -c -o join.o join.cc
-g++ -g -o simJoin free.o io.o main.o pre.o join.o
